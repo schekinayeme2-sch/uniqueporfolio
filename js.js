865be5ed840190b483bca4007b5460b545a0181b
schekina <schekinayeme2@gmail.com>
2026-04-22 18:04:11 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+const mots    = ["étudiante",  "alternante", "UX Designer",    "développeuse web"];
+const couleurs = ["#9333ea",   "#e11d48",    "#0ea5e9",        "#16a34a"];
+let i = 0;
+
+const el = document.getElementById("typing");
+
+function afficher() {
+  // Met le texte et la couleur
+  el.textContent = mots[i];
+  el.style.color = couleurs[i];
+
+  // Entrée : glisse vers le haut
+  el.classList.remove("hidden");
+  el.classList.add("visible");
+
+  // Après 2 secondes, sortie
+  setTimeout(sortir, 2000);
+}
+
+function sortir() {
+  el.classList.remove("visible");
+  el.classList.add("hidden");
+
+  // Après la transition, passe au mot suivant
+  setTimeout(() => {
+    i = (i + 1) % mots.length;
+    afficher();
+  }, 400); // correspond à la durée de la transition CSS
+}
+
+afficher();
